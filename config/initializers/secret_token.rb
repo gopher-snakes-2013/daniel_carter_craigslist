f79962058d93d704e9dc58686109d5ece1862720
Apprentice <apprentice@dbc-22.local>
2013-10-28 14:28:35 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarterDanielCraiglistz::Application.config.secret_token = '589cdf65d1a182fb13210ef9b3fb0b80f8185a624f2a13ea6e74e64b6d1bace9f5e227d595fd39e83557e010d6681a2140a19c4499664c8f5df81c91321c1809'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CarterDanielCraiglistz::Application.config.session_store :cookie_store, key: '_carter_daniel_craigListz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CarterDanielCraiglistz::Application.config.session_store :active_record_store
